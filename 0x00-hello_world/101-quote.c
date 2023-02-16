f64891889f21f07d003bc12c75a3ea6925bd010d
Mphare1 <thipanemp@gmail.com>
2023-02-16 14:57:21 -0800

    Advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char *the_chars = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *cr = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int length = 0;
 
-	while (the_chars[length] != '\0')
+	while (cr[length] != '\0')
 		length++;
 
-	write(2, the_chars, length);
+	write(2, cr, length);
 
 	return (1);
 }
