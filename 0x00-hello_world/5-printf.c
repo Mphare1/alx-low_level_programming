7158222e2db93c17ad1bd1ffe0204d5a87672340
Mphare1 <thipanemp@gmail.com>
2023-02-16 14:46:27 -0800

    the kegendary printif

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
 /**
  * main - Prints "with proper grammar, but the outcome is
  * a piece of art," followed by a new line.
- * Return: Always 0.
+ * Return: Always 0(succes).
+ *
  */
 
 int main(void)
