ded1a301ad6b2e1ae1cee16c7d4a213449f05d59
Mphare1 <thipanemp@gmail.com>
2023-02-16 04:50:09 -0800

    and the legendary printif

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
+
 /**
- * main - Entry point of the program
- *
- * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- *              followed by a new line using the printf function.
- *
- * Return: Always 0
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
+ * Return: Always 0.
  */
+
 int main(void)
 
 
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
