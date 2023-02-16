210fd73c8a7817a18531c262d9d5633e80ea2dc4
Mphare1 <thipanemp@gmail.com>
2023-02-16 05:11:58 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	write(2, str, len);
+
+	return (1);
+}
