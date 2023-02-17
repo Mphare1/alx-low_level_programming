8f73f2c4d62960e693d3827d4e15d3a0ede3b34f
Mphare1 <thipanemp@gmail.com>
2023-02-17 00:46:28 -0800

    pushing all files once because i am confident

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints whether a random number is positive or negative
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    printf("%d is ", n);
+    if (n > 0)
+    {
+        printf("positive\n");
+    }
+    else if (n == 0)
+    {
+        printf("zero\n");
+    }
+    else
+    {
+        printf("negative\n");
+    }
+
+    return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two digits
+ *
+ * Return: 0 on success
+ */
+int main(void) {
+    int i, j;
+
+    for (i = 0; i < 100; i++) {
+        for (j = i; j < 100; j++) {
+            putchar(i / 10 + '0');
+            putchar(i % 10 + '0');
+            putchar(' ');
+            putchar(j / 10 + '0');
+            putchar(j % 10 + '0');
+            if (i != 99 || j != 99) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+    return 0;
+}
