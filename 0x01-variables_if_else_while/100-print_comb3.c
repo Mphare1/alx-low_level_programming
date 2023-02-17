a7fd3b01cd8d77c151565a40a4a58485cd21f4f4
Mphare1 <thipanemp@gmail.com>
2023-02-17 13:44:15 -0800

    add this file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,29 +2,31 @@
 
 /**
  * main - Entry point
- *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-    int num1, num2;
 
-    for (num1 = 0; num1 < 10; num1++)
-    {
-        for (num2 = num1 + 1; num2 < 10; num2++)
-        {
-            putchar((num1 % 10) + '0');
-            putchar((num2 % 10) + '0');
+	int d, p;
 
-            if (num1 == 8 && num2 == 9)
-                continue;
+	for (d = '0'; d < '9'; d++)
+	{
 
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+	for (p = d + 1; p <= '9'; p++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
