bd5cb2feee508dcca2cf575e17d43a2569181677
Mphare1 <thipanemp@gmail.com>
2023-02-17 13:11:39 -0800

    print combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,24 +3,24 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-    int first, second;
+    int num1, num2;
 
-    for (first = 0; first < 9; first++)
+    for (num1 = 0; num1 < 10; num1++)
     {
-        for (second = first + 1; second <= 9; second++)
+        for (num2 = num1 + 1; num2 < 10; num2++)
         {
-            putchar((first % 10) + '0');
-            putchar((second % 10) + '0');
+            putchar((num1 % 10) + '0');
+            putchar((num2 % 10) + '0');
 
-            if (first != 8 || second != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
+            if (num1 == 8 && num2 == 9)
+                continue;
+
+            putchar(',');
+            putchar(' ');
         }
     }
 
