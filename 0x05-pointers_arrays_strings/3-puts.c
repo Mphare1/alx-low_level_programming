f22116a5cfb8dcae493290420b55e74d3d2deb16
Mphare1 <thipanemp@gmail.com>
2023-02-27 01:53:32 -0800

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,11 +8,11 @@
  */
 void _puts(char *str)
 {
-        int len = 0;
+int len = 0;
 
-        while (str[len])
-                len++;
+while (str[len])
+len++;
 
-        write(STDOUT_FILENO, str, len);
-        write(STDOUT_FILENO, "\n", 1);
+write(STDOUT_FILENO, str, len);
+write(STDOUT_FILENO, "\n", 1);
 }
