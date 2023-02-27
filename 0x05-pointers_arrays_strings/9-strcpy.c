8c2eec9a23b10e734dad0088d0bd863cf99b39a0
Mphare1 <thipanemp@gmail.com>
2023-02-27 12:00:36 -0800

    commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,20 @@
-#include "main.h"
-
 /**
- *   _strcpy - Copy a string
- *   @dest: Destination value
- *   @src: Source value
- *   Return: the pointer to dest
+ * _strcpy - Copies a string to a buffer
+ * @dest: Pointer to buffer
+ * @src: Pointer to string to be copied
+ *
+ * Return: Pointer to buffer
  */
-
 char *_strcpy(char *dest, char *src)
 {
-int i;
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-dest[i++] = '\0';
-return (dest);
+        int i;
+
+        for (i = 0; src[i] != '\0'; i++)
+        {
+                dest[i] = src[i];
+        }
+
+        dest[i] = '\0';
+
+        return (dest);
 }
