9a4ad3ec2d47a19aa7ef474fc16add8f5e6a2cb5
Mphare1 <123340673+Mphare1@users.noreply.github.com>
2023-03-02 08:06:32 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,13 @@ char *_strcat(char *dest, char *src)
     char *start = dest;
 
     while (*dest != '\0')
-        dest++;
+    dest++;
 
     while (*src != '\0')
     {
-        *dest = *src;
-        dest++;
-        src++;
+    *dest = *src;
+    dest++;
+    src++;
     }
 
     *dest = '\0';
