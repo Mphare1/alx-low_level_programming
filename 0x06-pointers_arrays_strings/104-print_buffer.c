41a7fec41b3b77be763cefff64d23c8ad180fc75
Mphare1 <thipanemp@gmail.com>
2023-03-02 00:22:22 -0800

    fixes

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,43 +1,46 @@
 /**
  * print_buffer - prints the content of a buffer
- * @b: the buffer to be printed
- * @size: the number of bytes to be printed
+ * @b: the buffer to print
+ * @size: the number of bytes to print
  *
- * Return: void
+ * Return: nothing
  */
 void print_buffer(char *b, int size)
 {
-	int i, j, k;
+    int i, j;
 
-	if (size <= 0)
-		printf("\n");
+    if (size <= 0)
+    {
+        printf("\n");
+        return;
+    }
 
-	for (i = 0; i < size; i += 10)
-	{
-		printf("%08x: ", i);
+    for (i = 0; i < size; i += 10)
+    {
+        printf("%08x: ", i);
 
-		for (j = i; j < i + 10; j++)
-		{
-			if (j < size)
-				printf("%02x", *(b + j));
-			else
-				printf("  ");
+        for (j = 0; j < 10; j++)
+        {
+            if (j + i >= size)
+                printf("  ");
+            else
+                printf("%02x", *(b + i + j));
 
-			if (j % 2 != 0)
-				printf(" ");
-		}
+            if (j % 2 != 0)
+                printf(" ");
+        }
 
-		for (k = i; k < i + 10; k++)
-		{
-			if (k >= size)
-				break;
+        for (j = 0; j < 10; j++)
+        {
+            if (j + i >= size)
+                break;
 
-			if (*(b + k) >= ' ' && *(b + k) <= '~')
-				printf("%c", *(b + k));
-			else
-				printf(".");
-		}
+            if (*(b + i + j) >= ' ' && *(b + i + j) <= '~')
+                printf("%c", *(b + i + j));
+            else
+                printf(".");
+        }
 
-		printf("\n");
-	}
+        printf("\n");
+    }
 }
