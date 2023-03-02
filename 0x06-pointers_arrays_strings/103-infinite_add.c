efe68f351e71b6fd10f32220912c3c638eebdab5
Mphare1 <thipanemp@gmail.com>
2023-03-02 00:11:17 -0800

    fixes

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,40 +1,40 @@
 /**
- * infinite_add - Adds two numbers
- * @n1: First number to add
- * @n2: Second number to add
- * @r: Buffer to store result
- * @size_r: Size of buffer
- *
- * Return: Pointer to the result, or 0 if result can't be stored in r
+ * infinite_add - Adds two numbers.
+ * @n1: First number.
+ * @n2: Second number.
+ * @r: Buffer to store result.
+ * @size_r: Buffer size.
+ * Return: Pointer to the result.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-        int i, j, k, len1, len2, sum, carry = 0;
+	int len1 = 0, len2 = 0, i = 0, j = 0, sum = 0, carry = 0;
 
-        for (len1 = 0; n1[len1]; len1++)
-                ;
-        for (len2 = 0; n2[len2]; len2++)
-                ;
-        if (len1 > size_r || len2 > size_r)
-                return (0);
-        for (i = len1 - 1, j = len2 - 1, k = 0; i >= 0 || j >= 0 || carry; i--, j--, k++)
-        {
-                sum = carry;
-                if (i >= 0)
-                        sum += n1[i] - '0';
-                if (j >= 0)
-                        sum += n2[j] - '0';
-                r[k] = sum % 10 + '0';
-                carry = sum / 10;
-        }
-        if (k >= size_r)
-                return (0);
-        r[k] = '\0';
-        for (i = 0, j = k - 1; i < j; i++, j--)
-        {
-                sum = r[i];
-                r[i] = r[j];
-                r[j] = sum;
-        }
-        return (r);
+	while (n1[len1])
+		len1++;
+
+	while (n2[len2])
+		len2++;
+
+	if (len1 + 2 > size_r || len2 + 2 > size_r || size_r < 2)
+		return (0);
+
+	i = len1 - 1, j = len2 - 1;
+	r[size_r - 1] = '\0';
+
+	while (i >= 0 || j >= 0 || carry != 0)
+	{
+		sum = carry;
+
+		if (i >= 0)
+			sum += n1[i--] - '0';
+
+		if (j >= 0)
+			sum += n2[j--] - '0';
+
+		r[--size_r] = (sum % 10) + '0';
+		carry = sum / 10;
+	}
+
+	return (&r[size_r]);
 }
