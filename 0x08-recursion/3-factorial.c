dcefe7bd3489a519adb49cf7278b80f100a68c89
Mphare <thipanemp@gmail.com>
2023-03-08 04:53:22 -0800

    02

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+/**
+ * factorial - computes the factorial of a given number
+ * @n: the number to compute the factorial of
+ *
+ * Return: the factorial of n, or -1 if n is negative
+ */
+int factorial(int n)
+{
+    if (n < 0) {
+        return -1;  // error: factorial is undefined for negative numbers
+    }
+    if (n == 0) {
+        return 1;   // base case: factorial of 0 is 1
+    }
+    return n * factorial(n - 1); // recursive case: n! = n * (n-1)!
+}
