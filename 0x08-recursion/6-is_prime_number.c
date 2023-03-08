8611fc51be8ba064eeac69cd359dfc76f6fd856b
Mphare <thipanemp@gmail.com>
2023-03-08 06:17:47 -0800

    06

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+/**
+ * is_prime_number - checks if a number is a prime number
+ * @n: the number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+    /* Check for base cases */
+    if (n < 2)
+        return 0;
+    if (n == 2)
+        return 1;
+    /* Check if n is even */
+    if (n % 2 == 0)
+        return 0;
+    /* Check odd divisors */
+    return check_divisors(n, 3);
+}
+
+/**
+ * check_divisors - checks if a number is divisible by odd numbers
+ * starting from a given divisor
+ * @n: the number to check
+ * @divisor: the first divisor to check (must be odd)
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int check_divisors(int n, int divisor)
+{
+    if (n % divisor == 0)
+        return 0;
+    if (divisor * divisor > n)
+        return 1;
+    return check_divisors(n, divisor + 2);
+}
