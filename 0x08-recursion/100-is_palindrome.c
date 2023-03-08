cf9b8e5511c8ced077c989e68395d739722634e9
Mphare <thipanemp@gmail.com>
2023-03-08 10:53:55 -0800

    100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,18 @@
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+	
+	if (len <= 1)
+		return (1);
+
+	if (*s == *(s + len - 1))
+		return (is_palindrome(s + 1) && 1);
+
+	return (0);
+}
