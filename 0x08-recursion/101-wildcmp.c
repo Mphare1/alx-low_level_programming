fb590410839c77bf5447d0bdc23185c2148ab51e
Mphare <thipanemp@gmail.com>
2023-03-08 11:01:18 -0800

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,46 +1,28 @@
 /**
- * wildcmp - compares two strings and returns 1 if they can be considered identical,
- *           otherwise returns 0. The second string can contain the special character *,
- *           which can replace any string (including an empty string).
+ * wildcmp - compares two strings and returns 1 if they can be considered
+ * identical, otherwise return 0. This function allows the second string to
+ * contain the special character *, which can replace any string (including
+ * an empty string).
+ * @s1: first string to compare
+ * @s2: second string to compare (may contain * wildcard)
  *
- * @s1: the first string to compare
- * @s2: the second string to compare
- *
- * Return: 1 if the strings can be considered identical, otherwise 0
+ * Return: 1 if s1 and s2 can be considered identical, otherwise 0
  */
 int wildcmp(char *s1, char *s2)
 {
-    /* If both strings are empty, they are identical */
     if (*s1 == '\0' && *s2 == '\0')
         return (1);
 
-    /* If the second string starts with *, try to match the rest of the strings */
     if (*s2 == '*')
     {
-        /* Skip the * in the second string */
-        s2++;
+        if (*s1 == '\0')
+            return (wildcmp(s1, s2 + 1));
 
-        /* If the second string is empty, everything matches */
-        if (*s2 == '\0')
-            return (1);
-
-        /* Try to match the rest of the second string with the first string */
-        while (*s1 != '\0')
-        {
-            if (wildcmp(s1, s2))
-                return (1);
-
-            s1++;
-        }
-
-        /* If we got here, the second string doesn't match the first string */
-        return (0);
+        return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
     }
 
-    /* If the characters match, continue comparing */
     if (*s1 == *s2)
         return (wildcmp(s1 + 1, s2 + 1));
 
-    /* If the characters don't match and there is no *, the strings are not identical */
     return (0);
 }
