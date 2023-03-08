9a3ef8d7c2f12b4abeb0b748add7ca5de68eaa64
Mphare <thipanemp@gmail.com>
2023-03-08 04:41:21 -0800

    00

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,14 +3,16 @@
  * _puts_recursion - prints a string followed by a new line using recursion
  * @s: pointer to string to be printed
  */
+
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') /* base case: end of string */
-    {
-        _putchar('\n');
-        return;
-    }
-
-    _putchar(*s); /* print current character */
-    _puts_recursion(s + 1); /* call function with next character */
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
