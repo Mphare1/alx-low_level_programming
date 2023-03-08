7ea239bd9d35f0ca05cba4a3e0312c87604bb1a5
Mphare <thipanemp@gmail.com>
2023-03-08 04:50:21 -0800

    02

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,14 @@
 /**
- * _strlen_recursion - Returns the length of a string
- * @s: Pointer to the string
+ * _strlen_recursion - returns the length of a string.
+ * @s: pointer to the string.
  *
- * Return: Length of the string
+ * Return: length of the string.
  */
+
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return 0;
-
-	return (_strlen_recursion(s + 1) + 1);
+    if (*s == '\0')
+        return (0);
+    else
+        return (1 + _strlen_recursion(s + 1));
 }
