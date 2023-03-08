b661b41b3f2b9d6146b941ea7506ea917f5156ac
Mphare <thipanemp@gmail.com>
2023-03-08 05:58:42 -0800

    04

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: the base integer
+ * @y: the exponent integer
+ *
+ * Return: the result of x raised to y, or -1 if y is negative
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+        return (-1);
+
+    if (y == 0)
+        return (1);
+
+    return (x * _pow_recursion(x, y - 1));
+}
