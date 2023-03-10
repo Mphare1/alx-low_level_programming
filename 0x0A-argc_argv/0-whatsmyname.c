ea1ef8e6809d8dc012dc170c3be761d8bbed1835
Mphare <thipanemp@gmail.com>
2023-03-10 02:02:57 -0800

    0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: Prints the name of the program, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; argv[0][i] != '\0'; i++)
+	{
+		_putchar(argv[0][i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
