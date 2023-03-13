4870a888e60236c8746e263c10437f64edbb1d34
Mphare <thipanemp@gmail.com>
2023-03-13 02:06:54 -0700

    df

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * alloc_grid - nested loop to make grid
  * @width: width input
@@ -9,33 +10,31 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **array;
-	int i, n;
+    int **array;
+    int i, n;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
+    if (width <= 0 || height <= 0)
+        return (NULL);
 
-	array = malloc(sizeof(int) * height);
+    array = malloc(sizeof(int *) * height);
 
-	if (array == NULL)
-	{
-		free(array);
-		return (NULL);
-	}
+    if (array == NULL)
+        return (NULL);
 
-	for (i = 0; i < height; i++)
-	{
-		array[i] = malloc(sizeof(int) * width);
-		if (array == NULL)
-		{
-			free(array);
-			return (NULL);
-		}
+    for (i = 0; i < height; i++)
+    {
+        array[i] = malloc(sizeof(int) * width);
+        if (array[i] == NULL)
+        {
+            for (n = i - 1; n >= 0; n--)
+                free(array[n]);
+            free(array);
+            return (NULL);
+        }
 
-		for (n = 0; n < width; n++)
-		{
-			array[i][n] = 0;
-		}
-	}
-	return (array);
+        for (n = 0; n < width; n++)
+            array[i][n] = 0;
+    }
+
+    return (array);
 }
