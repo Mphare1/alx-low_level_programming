aad5d7fcadfd77c226de4f1183bcc6007f262717
Mphare1 <123340673+Mphare1@users.noreply.github.com>
2023-03-20 13:48:53 +0200

    03

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3_FUNCTION_LIKE_MARCO_H_
-#define _3_FUNCTION_LIKE_MARCO_H_
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#endif  _3_FUNCTION_LIKE_MARCO_H_
+#ifndef _HEADER_
+#define _HEADER_
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
+#endif
