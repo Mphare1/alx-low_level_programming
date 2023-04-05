f5b69b66a11a651be7d76c83c34b5c53e7f043e2
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-05 22:13:46 +0200

    Create 8-sum_listint.c

---
+++ 0x13/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * get_sum_listint - returns the sum of all data in a linked list
+ * @head: pointer to the head node of the list
+ * Return: the sum of all data, or 0 if the list is empty
+ */
+int get_sum_listint(listint_t *head)
+{
+    int sum = 0;
+
+    while (head != NULL)
+    {
+        sum = sum + head->n;
+        head = head->next;
+    }
+
+    return sum;
+}
