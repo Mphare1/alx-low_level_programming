632b459fc3b74b284dce564a030f21f6f3c46337
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-09 21:52:00 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,36 +13,36 @@
 
 * Return: number of chars printed.
 */
-    ssize_t read_textfile(const char *filename, size_t letters)
-    {
-    int file_descriptor, error, read_count;
-    char *buffer;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int file_descriptor, error, read_count;
+char *buffer;
 
-    file_descriptor = error = read_count = 0;
-    if (!filename || !letters)
-    return (0);
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor < 0)
-    return (0);
+file_descriptor = error = read_count = 0;
+if (!filename || !letters)
+return (0);
+file_descriptor = open(filename, O_RDONLY);
+if (file_descriptor < 0)
+return (0);
 
-    buffer = malloc(sizeof(char) * letters + 1);
-    if (!buffer)
-    return (0);
-    read_count = read(file_descriptor, buffer, letters);
-    if (read_count < 0)
-    {
-    free(buffer);
-    return (0);
-    }
-    buffer[letters] = '\0';
-    error = write(STDOUT_FILENO, buffer, read_count);
-    if (error <= 0)
-    {
-    free(buffer);
-    return (0);
-    }
+buffer = malloc(sizeof(char) * letters + 1);
+if (!buffer)
+return (0);
+read_count = read(file_descriptor, buffer, letters);
+if (read_count < 0)
+{
+free(buffer);
+return (0);
+}
+buffer[letters] = '\0';
+error = write(STDOUT_FILENO, buffer, read_count);
+if (error <= 0)
+{
+free(buffer);
+return (0);
+}
 
-    free(buffer);
-    close(file_descriptor);
-    return (read_count);
-    }
+free(buffer);
+close(file_descriptor);
+return (read_count);
+}
