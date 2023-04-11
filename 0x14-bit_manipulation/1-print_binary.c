4d175e1bd8f123a44809dfdce1c439ba214f2322
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-11 04:04:30 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 /**
- * print_binary - function descript
- * @n: convert to binary
- *......
+ * print_binary -777777777777777
+ * @n: 8888888888888888888
+ * -----------------
  */
 void print_binary(unsigned long int n)
 {
-	if (n>1)
-	{
-	print_binary(n>>1);
-	}
-putchar((n & 1)+'0');
+if (n>1)
+print_binary(n>>1);
+_putchar((n & 1)+ '0');
+	
 }
