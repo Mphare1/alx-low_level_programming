a176c8def628498bbeba7f170f0a54f54c1c5699
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-11 02:17:43 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,21 +16,31 @@ int create_file(const char *filename, char *text_content)
 	int err_c =0, x=0, file_des=0;
 	if (!filename)
 	{
+		
 	return (-1);
+		
 	}
 	file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	
 	if (file_des < 0)
 	{
+		
 	return (-1);
+	
 	}
 	while (text_content && text_content[x])
 	x++;
 
 	err_c = write(file_des, text_content, x);
+	
 	if (err_c < 0)
 	{
+		
 	return (-1);
+		
 	}
+	
 	close(file_des);
+	
 	return (1);
 }
