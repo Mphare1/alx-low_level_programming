d6d47aa0fdbde7edfd56cdf2ebde50cae6e233e4
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-11 03:46:44 +0200

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,38 +9,49 @@
 
 static ssize_t read_file(char *file, char **buf, int fd);
 static void write_copy(char *file, int fd, char *buf, int len);
+/**
+ * main - ............
+ * @argc: c..........
+ * @argv: ............
+ *
+ * Return: 0..........
+ */
 
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t rd_len, close_err;
-	char *buf, *file_from;
+	ssize_t rd_len = 1, close_err;
+	char *buf = NULL, *file_from;
 	char *file_to;
-
-	buf = NULL;
-	rd_len = 1;
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = argv[1];
+	
 	file_to = argv[2];
+	
 	fd_from = open(file_from, O_RDONLY);
+	
 	if (fd_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		
 		exit(98);
 	}
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	
 	if (fd_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		
 		exit(99);
 	}
 	while (rd_len > 0)
 	{
 		rd_len = read_file(file_from, &buf, fd_from);
+		
 		if (!rd_len)
 			break;
 
@@ -48,17 +59,23 @@ int main(int argc, char *argv[])
 	}
 
 	free(buf);
+	
 	close_err = close(fd_from);
+	
 	if (close_err < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd_from);
+		
 		exit(100);
+		
 	}
 	close_err = close(fd_to);
 	if (close_err < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd_to);
-		exit(100);
+	
+	dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd_to);
+		
+	exit(100);
 	}
 	return (0);
 }
@@ -66,33 +83,42 @@ int main(int argc, char *argv[])
 static ssize_t read_file(char *file, char **buf, int fd)
 {
 	ssize_t rd_len;
-	char *tmp_buf;
+	
+	char *tmp_buf11;
 
 	tmp_buf = malloc(sizeof(char) * BUFFER_SIZE);
-	if (tmp_buf == NULL)
+	
+	if (tmp_buf11 == NULL)
+		
 		exit(99);
 
-	rd_len = read(fd, tmp_buf, BUFFER_SIZE);
+	rd_len = read(fd, tmp_buf11, BUFFER_SIZE);
+	
 	if (rd_len < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		
 		exit(98);
 	}
-	*buf = tmp_buf;
+	*buf = tmp_buf11;
 
 	return (rd_len);
 }
 
 static void write_copy(char *file, int fd, char *buf, int len)
 {
-	ssize_t wr_len;
-	int wr_err;
+	ssize_t wr_len1;
+	
+	int wr_err1;
 
-	wr_len = write(fd, buf, len);
-	if (wr_len < 0 || wr_len != len)
+	wr_len1 = write(fd, buf, len);
+	
+	if (wr_len1 < 0 || wr_len1 != len)
 	{
-		wr_err = (wr_len < 0) ? 99 : 98;
+		wr_err1 = (wr_len1 < 0) ? 99 : 98;
+		
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		exit(wr_err);
+		
+		exit(wr_err1);
 	}
 }
