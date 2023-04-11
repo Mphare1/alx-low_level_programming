d85e366a23cda988f9feea4644ab37586167ec5e
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-11 03:33:29 +0200

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,95 +18,79 @@ static void write_copy(char *file, int fd, char *buf, int len);
  *
  * Return: 0
  */
-int main(int argc, char *argv[])
-{
-	int fd1_in1, fd1_out1;
-	int rd1_len1, close1_err1;
-	char *buf1, *file1_from1;
-	char *file1_to1;
-
-	buf1 = NULL;
-	rd1_len1 = 1;
-if (argc != 3)
-{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
-}
-file1_from1 = argv[1];
-file1_to1 = argv[2];
-fd1_in1 = open(file1_from1, O_RDONLY);
-fd1_out1 = open(file1_to1, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-while (rd1_len1>0)
-{
-	rd1_len1 = read_file(file1_from1, &buf1, fd1_in1);
-	if (!rd1_len1)
-	break;
-		
-	write_copy(file1_to1, fd1_out1, buf1, rd1_len1);
-}
-
-free(buf1);
-close_err = close(fd1_in1);
-if (close_err < 0)
-{
-	dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd_in);
-	exit(100);
-}
-close1_err1 = close(fd_out);
-if (close1_err1 < 0)
-{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd_out);
-		exit(100);
-}
-return (0);
-}
-
-/**
- * read_file - Reads a file
- * @file: the file to read
- * @buf: buffer
- * @fd: file...
- *
- * Return: bytes
- */
 static ssize_t read_file(char *file, char **buf, int fd)
 {
-	ssize_t rd_len;
-	char *tmp_buf;
+	ssize_t read_len;
+	char *temp_buf;
 
-	tmp_buf = malloc(sizeof(char) * BUFFER_SIZE);
-	if (tmp_buf == NULL)
+	temp_buf = malloc(sizeof(char) * BUFFER_SIZE);
+	if (temp_buf == NULL)
 		exit(99);
 
-	rd_len = read(fd, tmp_buf, BUFFER_SIZE);
-	if (rd_len < 0)
+	read_len = read(fd, temp_buf, BUFFER_SIZE);
+	if (read_len < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 		exit(98);
 	}
-	*buf = tmp_buf;
+	*buf = temp_buf;
 
-	return (rd_len);
+	return (read_len);
 }
 
-/**
- * write_copy - Writes to file
- * @file: file to write to
- * @fd: file descript
- * @buf: buffer
- * @len: length
- *
- */
 static void write_copy(char *file, int fd, char *buf, int len)
 {
-	ssize_t wr_len;
-	int wr_err;
+	ssize_t write_len;
+	int write_err;
 
-	wr_len = write(fd, buf, len);
-	if (wr_len < 0 || wr_len != len)
+	write_len = write(fd, buf, len);
+	if (write_len < 0 || write_len != len)
 	{
-		wr_err = (wr_len < 0) ? 99 : 98;
+		write_err = (write_len < 0) ? 99 : 98;
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		exit(wr_err);
+		exit(write_err);
 	}
 }
+
+int main(int argc, char *argv[])
+{
+	int src_fd, dest_fd;
+	int close_err;
+	char *buf, *src_file;
+	char *dest_file;
+
+	buf = NULL;
+	int read_len = 1;
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp src_file dest_file\n");
+		exit(97);
+	}
+	src_file = argv[1];
+	dest_file = argv[2];
+	src_fd = open(src_file, O_RDONLY);
+	dest_fd = open(dest_file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while (read_len > 0)
+	{
+		read_len = read_file(src_file, &buf, src_fd);
+		if (!read_len)
+			break;
+		write_copy(dest_file, dest_fd, buf, read_len);
+	}
+
+	free(buf);
+	close_err = close(src_fd);
+	if (close_err < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", src_fd);
+		exit(100);
+	}
+	close_err = close(dest_fd);
+	if (close_err < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", dest_fd);
+		exit(100);
+	}
+	return (0);
+}
+
