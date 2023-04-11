026ea353a59bf3570ab6fdd316d70a5a4f482212
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-11 02:16:18 +0200

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,29 +5,32 @@
 #include <fcntl.h>
 
 /**
- * create_file - this will  create a file
- * @filename: ...
- * @text_content: things to add to the file.
+ * create_file - ....... create a file
+ * @filename: ************
+ * @text_content: ******** add to the file.
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 **** or -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int err_code, len, file_descriptor;
-
-	err_code = len = file_descriptor = 0;
+	int err_c =0, x=0, file_des=0;
 	if (!filename)
-		return (-1);
+	{
+	return (-1);
+	}
+	file_des = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file_des < 0)
+	{
+	return (-1);
+	}
+	while (text_content && text_content[x])
+	x++;
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-		if (file_descriptor < 0)
-			return (-1);
-	while (text_content && text_content[len])
-		len++;
-
-	err_code = write(file_descriptor, text_content, len);
-	if (err_code < 0)
-		return (-1);
-	close(file_descriptor);
+	err_c = write(file_des, text_content, x);
+	if (err_c < 0)
+	{
+	return (-1);
+	}
+	close(file_des);
 	return (1);
 }
