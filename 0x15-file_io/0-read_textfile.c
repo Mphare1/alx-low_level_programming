7fda940b25aee0a77470e2dcdc28bcebd3e75063
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-11 02:04:36 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,43 +6,51 @@
 #include <fcntl.h>
 
 /**
-* read_textfile - fuction that read a text file and print it out
-* the POSIX std out
-* @filename: body of text to print.
-* @letters: max char to print.
+* read_textfile - read a text file and print it
+* 
+* @filename: .......
+* @letters: ... char to print.
 
-* Return: number of chars printed.
+* Return: .... chars printed.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int file_descriptor, error, read_count;
-char *buffer;
+int file_des = 0 erro = 0, read_c = 0;
+char *buff;
 
-file_descriptor = error = read_count = 0;
 if (!filename || !letters)
-return (0);
-file_descriptor = open(filename, O_RDONLY);
-if (file_descriptor < 0)
-return (0);
-
-buffer = malloc(sizeof(char) * letters + 1);
-if (!buffer)
-return (0);
-read_count = read(file_descriptor, buffer, letters);
-if (read_count < 0)
 {
-free(buffer);
 return (0);
 }
-buffer[letters] = '\0';
-error = write(STDOUT_FILENO, buffer, read_count);
-if (error <= 0)
+  file_des = open(filename, O_RDONLY);
+  if (file_des < 0)
 {
-free(buffer);
+  return (0);
+}
+  if (!buff)
+{
+  return (0);
+}
+  read_c = read(file_des, buffer, letters);
+  if (read_c < 0)
+  {
+  free(buff);
+  return (0);
+  }
+  buff[letters] = '\0';
+  erro = write(STDOUT_FILENO, buff, read_c);
+  if (error <= 0)
+  {
+  free(buff);
+  
 return (0);
 }
 
-free(buffer);
-close(file_descriptor);
-return (read_count);
+free(buff);
+  
+close(file_des);
+  
+return (read_c);
+  
+ 
 }
