f610e49697d735f7217a79994ddc1b0587e3944c
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-28 14:29:47 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,40 +16,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int _fd, _errr, _rd;
-	
-	char *_buf;
+int _fd, _errr, _rd;
+char *_buf;
 
-	_fd = _errr = _rd = 0;
-	if (!filename || !letters)
-		return (0);
+_fd = _errr = _rd = 0;
+if (!filename || !letters)
+return (0);
 	
-	_fd = open(filename, O_RDONLY);
-	if (_fd < 0)
-		return (0);
+_fd = open(filename, O_RDONLY);
+if (_fd < 0)
+return (0);
 
-	_buf = malloc(sizeof(char) * letters + 1);
+_buf = malloc(sizeof(char) * letters + 1);
 	
-	if (!_buf)
-		return (0);
-	_rd = read(_fd, _buf, letters);
-	if (_rd < 0)
-	{
-		free(_buf);
-		return (0);
-	}
-	_buf[letters] = '\0';
+if (!_buf)
+return (0);
+_rd = read(_fd, _buf, letters);
+if (_rd < 0)
+{
+free(_buf);
+return (0);
+}
+_buf[letters] = '\0';
 	
-	_errr = write(STDOUT_FILENO, _buf, _rd);
-	if (_errr <= 0)
-	{
-		free(_buf);
-		return (0);
-	}
-
-	free(_buf);
+_errr = write(STDOUT_FILENO, _buf, _rd);
+if (_errr <= 0)
+{
+free(_buf);
+return (0);
+}
+free(_buf);
 	
-	close(_fd);
+close(_fd);
 	
-	return (_rd);
+return (_rd);
 }
