a248bf3a9abeb9c582e95f2b1dd9ade2867fe89c
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-04-28 14:26:10 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,34 +16,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, err, rd;
-	char *buf;
+	int _fd, _errr, _rd;
+	
+	char *_buf;
 
-	fd = err = rd = 0;
+	_fd = _errr = _rd = 0;
 	if (!filename || !letters)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	
+	_fd = open(filename, O_RDONLY);
+	if (_fd < 0)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters + 1);
-	if (!buf)
+	_buf = malloc(sizeof(char) * letters + 1);
+	
+	if (!_buf)
 		return (0);
-	rd = read(fd, buf, letters);
-	if (rd < 0)
+	_rd = read(_fd, _buf, letters);
+	if (_rd < 0)
 	{
 		free(buf);
 		return (0);
 	}
-	buf[letters] = '\0';
-	err = write(STDOUT_FILENO, buf, rd);
-	if (err <= 0)
+	_buf[letters] = '\0';
+	
+	errr = write(STDOUT_FILENO, _buf, _rd);
+	if (errr <= 0)
 	{
-		free(buf);
+		free(_buf);
 		return (0);
 	}
 
-	free(buf);
-	close(fd);
-	return (rd);
+	free(_buf);
+	
+	close(_fd);
+	
+	return (_rd);
 }
