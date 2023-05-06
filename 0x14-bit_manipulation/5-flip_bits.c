08ae5dd5717824f871048362013fde3d8720c008
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-06 04:30:28 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,11 +2,10 @@
 #include <conio.h>
 
 /**
- * flip_bits - returns the number of bits you would need to flip
- *             to get from one number to another.
- * @n: number to be flipped
- * @m: number to filp n to
- * Return: no. of bits to be flipped
+ * flip_bits -...
+ * @n: .......
+ * @m: ......
+ * Return: ......
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
@@ -14,11 +13,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 unsigned int bts = 0;
 unsigned long int to_xor = n ^ m;
     
-    while(to_xor)
-    {
-        bts += to_xor & 1;
-        to_xor >>= 1;
-    }
+while(to_xor)
+{
+bts += to_xor & 1;
+to_xor >>= 1;
+}
     
-    return (bts);
+return (bts);
 }
