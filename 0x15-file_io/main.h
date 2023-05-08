cbc2cf5a7e9eaba87b100a587a5c6098ed1cab0a
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-09 00:57:01 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,7 +14,7 @@
 int _putchar(char c);
 /*0-read_textfile.c*/
 ssize_t read_textfile(const char *filename, size_t letters);
-
+/*1-create_file.c*/
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
