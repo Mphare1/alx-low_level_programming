7b8edf3a9d84a94feb891b9bea61c2a15adda031
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-05-09 01:02:43 +0200

    Create 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+  * append_text_to_file - Appends text at the end
+  * @filename: Name of the file.
+  * @text_content: ...
+  * Return: 1 on success or -1 on failure
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int open_f, stats, counter = 0;
+    
+    if(filename == NULL)
+        return (-1);
+    if(text_content == NULL)
+        return (1);
+        
+    open_f = open(filename, O_APPEND | O_WRONLY);
+    if(fd == -1)  /*If it fails*/
+        return (-1);
+        
+    for(; text_content[counter] != '\0'; counter++)
+        ;
+    stats = write(open_f, text_content, counter);
+    
+    if(stats == -1)
+        return (-1);
+        
+    close(open_f);
+    
+    return (1);
+}
