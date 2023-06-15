269ed4b50b95ff3d24214f7631fdaf45d9f6632c
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-06-16 01:45:25 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position in a doubly linked list.
- * @h: Pointer to the head of the doubly linked list.
- * @idx: Index at which the new node should be inserted.
- * @n: Value to be stored in the new node.
+ * insert_dnodeint_at_index - ...
+ * @h: ...
+ * @idx: ...
+ * @n: ...
  *
- * Return: Pointer to the newly inserted node, or NULL on failure.
+ * Return: Pointer to the  inserted node or NULL on failure.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *old_node;
+	dlistint_t *new_nod, *old_nod;
+	
 	unsigned int i;
 
 	if (h == NULL)
@@ -19,28 +20,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	old_node = *h;
+	old_nod = *h;
 
-	for (i = 0; old_node != NULL && i < idx; i++)
-		old_node = old_node->next;
+	for (i = 0; old_nod != NULL && i < idx; i++)
+		old_nod = old_nod->next;
 
-	if (old_node == NULL && i == idx)
+	if (old_nod == NULL && i == idx)
 		return (add_dnodeint_end(h, n));
-	else if (old_node != NULL)
+	
+	else if (old_nod != NULL)
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
+		new_nod = malloc(sizeof(dlistint_t));
+		
+		if (new_nod == NULL)
 			return (NULL);
 
-		new_node->n = n;
-		new_node->prev = old_node->prev;
-		new_node->next = old_node;
+		new_nod->n = n;
+		new_nod->prev = old_nod->prev;
+		
+		new_nod->next = old_nod;
 
-		if (old_node->prev != NULL)
-			old_node->prev->next = new_node;
-		old_node->prev = new_node;
+		if (old_nod->prev != NULL)
+			old_nod->prev->next = new_nod;
+		
+		old_nod->prev = new_nod;
 
-		return (new_node);
+		return (new_nod);
 	}
 
 	return (NULL);
